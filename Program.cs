6478ae46283de954ef7e538366eb55d66a46ba93
nando32117@gmail.com <nando32117@gmail.com>
2024-01-04 17:10:02 -0300

    Desfio POO Dio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Android Nokia");
+Smartphone nokia = new Nokia(numero: "61982222256", modelo: "1234", imei: "15dade568d5", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Tinder");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone IOS Apple");
+Smartphone iphone = new Iphone(numero: "61983265458", modelo: "685648", imei: "654sadr895", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
